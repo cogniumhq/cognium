45b3e6e3fdcc2a56ecc717b4d3b2aa224a0a132c
Eyal Arasu <2180386+elarasu@users.noreply.github.com>
2026-02-17 17:22:58 -0800

    Release v1.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.4] - 2026-02-17
+
+### Changes
+
+- Patched
+  * upgrade circle-ir to latest
+
+[1.0.4]: https://github.com/cogniumhq/cognium/compare/v1.0.3...v1.0.4
+
 ## [1.0.0] - 2025-02-11
 
 ### Added
--- Formula/cognium.rb
+++ Formula/cognium.rb
@@ -2,18 +2,18 @@ class Cognium < Formula
   desc "AI-powered static analysis CLI for detecting security vulnerabilities"
   homepage "https://cognium.dev"
   url "https://github.com/cogniumhq/cognium/releases/download/v1.0.0/cognium-darwin-arm64"
-  sha256 "" # TODO: Add SHA256 hash after first release
-  version "1.0.0"
+  sha256 "3e4230513f8cafbb886b0db4b7c8df8af62dd43ed5fa9b96d41b24094b8137a0" # TODO: Add SHA256 hash after first release
+  version "1.0.4"
   license "MIT"
 
   # Supports macOS (Darwin) on both Intel and Apple Silicon
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/cogniumhq/cognium/releases/download/v1.0.0/cognium-darwin-arm64"
-      sha256 "" # TODO: Add SHA256 for ARM64
+      sha256 "3e4230513f8cafbb886b0db4b7c8df8af62dd43ed5fa9b96d41b24094b8137a0" # TODO: Add SHA256 for ARM64
     else
       url "https://github.com/cogniumhq/cognium/releases/download/v1.0.0/cognium-darwin-x64"
-      sha256 "" # TODO: Add SHA256 for x64
+      sha256 "90a0f90cc8d29c9aca956f2429905de1b219cb60562d16e5c9e5ffd9e0f751bc" # TODO: Add SHA256 for x64
     end
   end
 
@@ -21,10 +21,10 @@ class Cognium < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/cogniumhq/cognium/releases/download/v1.0.0/cognium-linux-arm64"
-      sha256 "" # TODO: Add SHA256 for ARM64
+      sha256 "d5adce258bfc77b8abff403cfbaa6c075b9f56b1d8e29f76905415abaac8124e" # TODO: Add SHA256 for ARM64
     else
       url "https://github.com/cogniumhq/cognium/releases/download/v1.0.0/cognium-linux-x64"
-      sha256 "" # TODO: Add SHA256 for x64
+      sha256 "3fed619f20c38e1aeb16eafbe36a36716abe6396322c59f989ee8a3e1dd3440b" # TODO: Add SHA256 for x64
     end
   end
 
